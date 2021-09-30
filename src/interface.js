29f5e817c7d9b4020d7c04c30b958e02e7dd7582
Amanda Vuong (she/her) <81902165+mandyvuong@users.noreply.github.com>
2021-09-30 20:20:36 +0100

    London temperature display

--- src/interface.js
+++ src/interface.js
@@ -2,6 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const updateTemperature = () => {
         document.querySelector('#temperature').innerText = thermostat.temp;
         document.querySelector('#temperature').className = thermostat.energyUsage();
+
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    document.querySelector('#current-temperature').innerText = data.main.temp;
+  });
       }
   
   const thermostat = new Thermostat();
