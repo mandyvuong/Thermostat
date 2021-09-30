df2b4c62257f68aec28477ab990f31091e00566b
Amanda Vuong (she/her) <81902165+mandyvuong@users.noreply.github.com>
2021-09-30 11:33:04 +0100

    DOM added, not css

---
+++ src/interface.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const updateTemperature = () => {
+      document.querySelector('#temperature').innerText = thermostat.temp;
+  }
+  
+  const thermostat = new Thermostat();
+  updateTemperature();
+
+  document.querySelector('#temperature-up').addEventListener('click', () => {
+      thermostat.up();
+      updateTemperature();
+  })
+
+  document.querySelector('#temperature-down').addEventListener('click', () => {
+      thermostat.down();
+      updateTemperature();
+  })
+
+  document.querySelector('#temperature-reset').addEventListener('click', () => {
+      thermostat.resetTemp();
+      updateTemperature();
+  })
+
+  document.querySelector('#powersaving-on').addEventListener('click', () => {
+      thermostat.switchPowerSavingModeOn();
+      updateTemperature();
+  })
+
+  document.querySelector('#powersaving-off').addEventListener('click', () => {
+      thermostat.switchPowerSavingModeOff();
+      updateTemperature();
+  })
+})
+
+// Comment - can't get colour display working
+// const updateTemperature = () => {
+//     document.querySelector('#temperature').innerText = thermostat.temp;
+//     document.querySelector('#temperature').className = thermostat.energyUsage();
+//   }
+  
+
